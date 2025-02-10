17cabc8fbf437b6e01e73e6c2db0a9b19b1483d4
ricfrst <ricardofurst06@gmail.com>
2025-02-09 21:48:27 -0500

    Main

--- clean_dt.py
+++ clean_dt.py
@@ -1,4 +1,18 @@
 import os
 from pathlib import Path
 
-desktop_path = Path.home() //"desktop"
+
+#get desktop path
+dt_path = Path.home()/"Desktop"
+
+#string to print
+#f to replace path var with actual path name, just easier to read
+print(f"The path in question : {dt_path}")
+
+#list dt items
+for item in dt_path.iterdir():
+    if item.is_dir():
+        print(f"FOLDER {item}")
+    elif item.is_file():
+        file_extension = item.suffix.lower()  # Get the file extension and make it lowercase
+        print(f"FILE {item}| Extension:{file_extension}")
